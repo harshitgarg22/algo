14fc480878a5fd55e8f8efabedfa1365bfce66da
harshitgarg22 <hg1229@gmail.com>
2019-10-13 11:42:22 +0530

    Fix header file for correct prototype. Included vector header file and use vector<int> as first parameter.

--- array_ops/choosePivot.h
+++ array_ops/choosePivot.h
@@ -1,6 +1,8 @@
 #ifndef CHOOSE_PIVOT_H_INCLUDED
 #define CHOOSE_PIVOT_H_INCLUDED
 
-unsigned long int choosePivot(unsigned long int, int method = 2);
+#include<vector>
+
+unsigned long int choosePivot(std::vector<int>, int method = 2);
 
 #endif
