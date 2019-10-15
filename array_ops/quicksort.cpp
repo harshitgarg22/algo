48e5396546642b328e5e89af39d13fcf2d4b347e
harshitgarg22 <hg1229@gmail.com>
2019-10-15 14:03:43 +0530

    Modified quicksort.cpp to work with partition.cpp

--- array_ops/quicksort.cpp
+++ array_ops/quicksort.cpp
@@ -1,10 +1,9 @@
 #include<vector>
-#include "./choosePivot.h"
+#include "./partition.h"
 
 using namespace std;
 
 unsigned long int quicksort(vector<int>&, int method=3);
-unsigned long int partition(vector<int>&, int method);
 
 unsigned long int quicksort(vector<int> &arr, int method){
 
@@ -16,53 +15,47 @@ unsigned long int quicksort(vector<int> &arr, int method){
   }
 
   else{
-    count += partition(arr, method);
+    unsigned long pivot;
+    pivot = partition(arr, method);
+
+    vector<int> a, b;
+
+    // saving the partition into a and b
+
+    for(unsigned long int i=0; i<arr.size(); i++){
+      if(arr.at(i)<arr.at(pivot)){
+        a.push_back(arr.at(i));
+      }
+      else if((arr.at(i)==arr.at(pivot))&&i!=pivot){
+        a.push_back(arr.at(i));
+      }
+      else if(arr.at(i)>arr.at(pivot)){
+        b.push_back(arr.at(i));
+      }
+    }
+
+    count += quicksort(a, method);
+    count += quicksort(b, method);
+    count += a.size() + b.size();
+
+    // reconstructing arr
+    int temp;
+    temp = arr.at(pivot);
+
+    arr.clear();
+
+    for(unsigned long int i=0; i<a.size(); i++){
+      arr.push_back(a[i]);
+    }
+
+    arr.push_back(temp);
+
+    for(unsigned long int i=0; i<b.size(); i++){
+      arr.push_back(b[i]);
+    }
+
   }
 
   return count;
 }
 
-unsigned long int partition(vector<int> &arr, int method){
-
-  unsigned long int pivot = choosePivot(arr, method);
-  unsigned long int count = 0;
-
-  int temp;
-  temp = arr.at(pivot);
-
-  vector<int> a, b;
-
-  for(unsigned long int i=0; i<arr.size(); i++){
-    if(arr.at(i)<arr.at(pivot)){
-      a.push_back(arr.at(i));
-    }
-    else if((arr.at(i)==arr.at(pivot))&&i!=pivot){
-      a.push_back(arr.at(i));
-    }
-  }
-
-  for(unsigned long int i=0; i<arr.size(); i++){
-    if(arr.at(i)>arr.at(pivot)){
-      b.push_back(arr.at(i));
-    }
-  }
-
-  count += quicksort(a, method);
-  count += quicksort(b, method);
-
-  arr.clear();
-
-  for(unsigned long int i=0; i<a.size(); i++){
-    arr.push_back(a[i]);
-  }
-
-  arr.push_back(temp);
-
-  for(unsigned long int i=0; i<b.size(); i++){
-    arr.push_back(b[i]);
-  }
-
-  count += a.size() + b.size();
-
-  return count;
-}
